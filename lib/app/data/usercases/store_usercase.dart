3afee127c29c206ab4542135407cc9e6dcdef688
Trịnh Linh <trinhhoailinh201@gmail.com>
2023-02-26 12:45:56 +0700

    initical commit

--- lib/app/data/usercases/store_usercase.dart
+++ lib/app/data/usercases/store_usercase.dart
@@ -2,32 +2,10 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:new_wewo/app/data/models/store_model.dart';
+import 'package:new_wewo/app/data/repository/store_repository.dart';
 
-class StoreUserCase {
+class StoreUseCase {
   Future<List<Store>> fetchAllStore() async {
-    final Uri uri = Uri(
-      scheme: 'https',
-      host: 'wewo-store.vercel.app',
-      path: '/api/seller/get-store-by-sellerid',
-      queryParameters: {
-        'seller_id': '63ae60fc7a68919cfbb7f747',
-      },
-    );
-    final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      return getStore(response.body);
-    } else {
-      // throw Exception('Failed to load album');
-      return [];
-    }
-  }
-
-  List<Store> getStore(String body) {
-    final List<Store> stores = [];
-    final List<dynamic> data = jsonDecode(body)['data'];
-    for (var element in data) {
-      stores.add(Store.fromJson(element));
-    }
-    return stores;
+    return await StoreRepository().fetchAllStore();
   }
 }
