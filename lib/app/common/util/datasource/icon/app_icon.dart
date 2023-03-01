3c58e4e74a3e98d1116ee6d7cc36f153a5cf3707
Sucanabo <95233836+felixdinh@users.noreply.github.com>
2023-03-01 15:32:26 +0700

    create app_icon

---
+++ lib/app/common/util/datasource/icon/app_icon.dart
@@ -0,0 +1,57 @@
+class AppIcons{
+  const AppIcons._();
+
+  static const String iconPath = 'assets/icons';
+  static const String systemPath = '$iconPath/system';
+  static const String productPath = '$iconPath/product';
+
+  // Alert Icon
+  static const alertCancel = '$iconPath/alert_cancel.svg';
+  static const alertSuccess = '$iconPath/alert_success.svg';
+  static const alertWarning = '$iconPath/alert_warning.svg';
+
+  // Social Icon
+  static const apple = '$iconPath/apple.svg';
+  static const facebook = '$iconPath/facebook.svg';
+  static const google = '$iconPath/google.svg';
+  static const googlePlus = '$iconPath/google-plus.svg';
+  static const paypal = '$iconPath/paypal.svg';
+
+  // System Icon
+  static const alert = '$iconPath/alert.svg';
+  static const bag = '$iconPath/bag.svg';
+  static const bank = '$iconPath/bank.svg';
+  static const bottom = '$iconPath/bottom.svg';
+  static const cart = '$iconPath/cart.svg';
+  static const creditCard = '$iconPath/credit_card.svg';
+  static const date = '$iconPath/date.svg';
+  static const filter = '$iconPath/filter.svg';
+  static const gender = '$iconPath/gender.svg';
+  static const home = '$iconPath/home.svg';
+  static const left = '$iconPath/left.svg';
+  static const list = '$iconPath/list.svg';
+  static const location = '$iconPath/location.svg';
+  static const love = '$iconPath/love.svg';
+  static const message = '$iconPath/message.svg';
+  static const mic = '$iconPath/mic.svg';
+  static const minus = '$iconPath/minus.svg';
+  static const more = '$iconPath/more.svg';
+  static const notification = '$iconPath/notification.svg';
+  static const offer = '$iconPath/offer.svg';
+  static const password = '$iconPath/password.svg';
+  static const phone = '$iconPath/phone.svg';
+  static const plus = '$iconPath/plus.svg';
+  static const right = '$iconPath/right.svg';
+  static const search = '$iconPath/search.svg';
+  static const short = '$iconPath/short.svg';
+  static const trash = '$iconPath/trash.svg';
+  static const user = '$iconPath/user.svg';
+  static const username = '$iconPath/username.svg';
+  static const x = '$iconPath/x.svg';
+  static const transaction = '$iconPath/transaction.svg';
+
+  // Product Icon
+  static const bikini = '$iconPath/transaction.svg';
+
+
+}
